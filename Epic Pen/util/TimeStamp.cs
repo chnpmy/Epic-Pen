b07721f83881e3b013a7da258c8e14ca88833ef3
chnpmy <chnpmy@gmail.com>
2014-12-26 15:59:39 +0800

    加入一个时间戳生成器

---
+++ Epic Pen/util/TimeStamp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Epic_Pen.util
+{
+    class TimeStamp
+    {
+        public static int Current = GetCurrentTimeStamp();
+        /**
+         * 得到当前时间相对于1970/1/1的时间戳，以秒为单位，可保证在公元2038年前不会溢出
+         */
+        private static int GetCurrentTimeStamp()
+        {
+            DateTime startTime = TimeZone.CurrentTimeZone.ToLocalTime(new DateTime(1970, 1, 1, 0, 0, 0));
+            TimeSpan delta = DateTime.Now.Subtract(startTime);
+            return Convert.ToInt32(Math.Round(delta.TotalSeconds, MidpointRounding.AwayFromZero));
+        }
+    }
+}
